8ce4b4134750149fe79236417e886bddeb9e03f9
Mecily <Mecily@gmail.com>
2020-08-26 16:14:23 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/android/cc/mecily/envtest/db/Province.java
@@ -0,0 +1,31 @@
+package com.android.cc.mecily.envtest.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Mecily on 2020/8/26.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
